0e9ef80ce5bd60e256ac5d2e87aee0c034d00528
Stef Tervelde <stef@steftervelde.nl>
2020-10-06 14:57:28 +0200

    next up, deployment :(

--- plugins/figma/main.ts
+++ plugins/figma/main.ts
@@ -4,6 +4,7 @@
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
+import "@figma/plugin-typings";
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
