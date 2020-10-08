18552f2ac666b97ecf841a59c2ed8d0735c28bbe
Stef Tervelde <stef@steftervelde.nl>
2020-10-08 15:41:07 +0200

    cookies don't work inside the data iframe

--- plugins/figma/ui.js
+++ plugins/figma/ui.js
@@ -1,11 +1,7 @@
 import Vue from "vue";
 import UI from "./components/ui";
-import VueCookies from "vue-cookies";
 
 let vm = new Vue({
   el: "#app",
   extends: UI,
 });
-
-Vue.use(VueCookies);
-Vue.$cookies.config("7d");
