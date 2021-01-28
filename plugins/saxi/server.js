7f478c4b1852142d49b93934a7db244228526071
Stef Tervelde <stef@steftervelde.nl>
2021-01-28 15:57:22 +0100

    remove unused file

--- plugins/saxi/server.js
+++
@@ -1,7 +0,0 @@
-import saxi from "saxi";
-
-export default async function(args) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  if (this.nuxt.options._build && !this.options.dev) return;
-  saxi.server.startServer(this.nuxt.options.saxiPort);
-}
